0da5ac4cac5189c829c7c6b6ce7208f9fd30416d
Brian Dombrowski <bdombro@gmail.com>
2018-04-24 16:16:33 -0400

    Snapshot

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # chrome-bulk-screenshot
+
+## About
+
+This package will take pdf snapshots of rendered web pages in bulk using Puppeteer (aka headless Chromium).
+
+## Why?
+
+Using the chrome cli over many URLs is way CPU expensive and slow, because you must open a new browser per URL. This app opens and re-uses a single browser instance, thereby limiting to a single CPU AND with less CPU effort.
+
+
+## Caveats
+
+1. Takes about 2 seconds per snapshot per screen
+1. Not quite pixel perfect.
+
+
+## Requirements
+
+1. Requires Node v7.x or higher
+
+
+## Usage
+
+1. Copy and configure the sample yaml to your project root directory.
+
+2. Run this:
+```
+npm i
+node chrome-bulk-screenshot.js
+```
+
+Then check the 'out' folders.
+
+
+## Contributing
+
+Open up a pull request or email me
+
+
+## Related
+
+1. [headless-bulk-screenshot](https://github.com/rustyy/headless-bulk-screenshot) - Opens a new browser instance per snapshot -- less CPU effecient.
---
+++ index.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env Node
+
+/**
+ * Dependencies
+ * Requires Node v7.x or higher
+ */
+
+const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
+const YAML = require('yamljs');
+
+/**
+ * Config
+ */
+
+if (fs.existsSync('config.yml'))
+    var config = YAML.load('config.yml');
+else 
+    var config = YAML.load('config-example.yml');
+
+
+/**
+ * Functions
+ */
+
+// Pads a number. For example, PadNumber(2, 4) returns 0002
+function PadNumber(num, padlen, padchar) {
+    var pad_char = typeof padchar !== 'undefined' ? padchar : '0';
+    var pad = new Array(1 + padlen).join(pad_char);
+    return (pad + num).slice(-pad.length);
+}
+function UrlSlugify(url) {
+    return url
+        .replace(/https:\/\//, '')
+        .replace(/http:\/\//, '')
+        .replace(/\//g, '!')
+        // .replace(/[<>:"\/\\|?*\x00-\x1F]/g, '-') // MSWindows safe
+        .replace(/[<>:"\/\\|*\x00-\x1F]/g, '-')
+}
+function ResetDir(path) {
+    if (fs.existsSync(path)) { fs.removeSync(path); } fs.mkdirSync(path);
+}
+
+
+
+/**
+ * Parse Config
+ */
+
+// Prepare output directory
+if (config.outPdfDir) ResetDir(config.outPdfDir);
+if (config.outJpgDir) ResetDir(config.outJpgDir);
+
+// Parse urls
+config.urls = config.urlsRaw.replace( /\t/g, '' ).replace( / /g, '' ).replace( /\n/g, " " ).split( " " );
+
+
+/**
+ * Main
+ */
+
+(async() => {
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+for (let [i,url] of config.urls.entries()) {
+    var snapshotFileName = PadNumber(i, config.padding) + '.' + UrlSlugify(url);
+
+    console.log("Getting " + snapshotFileName);
+
+    await page.emulateMedia('screen');
+    await page.goto(url, {waitUntil: 'networkidle' + config.networkIdleLevel}); // networkidle0 = wait until 0 connections left; networkidel2 = wait until 2 connections left.
+
+    for (let screenSize of config.screenSizes) {
+
+        if (config.outJpgDir) {
+            await page.setViewport({width:screenSize, height:1024 });
+            await page.screenshot({
+                path: config.outJpgDir + '/' + snapshotFileName + '.' + screenSize + '.jpg', 
+                fullPage:true, 
+                quality:config.quality 
+            });
+        }
+        
+        if (config.outPdfDir) {
+            await page.pdf({
+                path: config.outPdfDir + '/' + snapshotFileName + '.' + screenSize + '.pdf',
+                width:screenSize, 
+                displayHeaderFooter:true, 
+                printBackground:true, 
+            });
+        }
+    }
+}
+
+await browser.close();
+
+})();
