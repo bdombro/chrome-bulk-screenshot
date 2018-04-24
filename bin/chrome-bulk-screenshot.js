901475d4b55702bab5d4b34836f681cbe898a186
Brian Dombrowski <bdombro@gmail.com>
2018-04-24 18:52:48 -0400

    Bugfix

--- bin/chrome-bulk-screenshot.js
+++ bin/chrome-bulk-screenshot.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env Node
+#!/usr/bin/env node
 
 const chromeBulkScreenshot = require('chrome-bulk-screenshot');
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env Node
+#!/usr/bin/env node
 
 /**
  * Dependencies
